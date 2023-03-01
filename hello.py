2f37af543acc98a95b485ee22271c5c4582d1388
Tassaing <d.leveille@hotmail.fr>
2023-03-01 06:51:06 +0100

    testip

--- hello.py
+++ hello.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask import json
 import datetime
-import socket
 
 app = Flask(__name__)
 
 uptime = f"up since {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
 version="0.0.1"
 
-
+import socket
 
 
 @app.route("/")
@@ -16,7 +15,7 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     s.connect(('<broadcast>', 0))
-    return "Hello Guys! V2", print (getNetworkIp())
+    return "Hello Guys! V2", getNetworkIp()
 
 @app.route("/healthz")
 def health():
